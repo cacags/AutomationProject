8708a7d70914b9466c377311e063f9c70d49a171
Caglar Keskin <caglarkeskin@gmail.com>
2020-09-30 22:45:36 +0200

    initial commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
         System.out.println("This is just for to understand Git and Github");
         System.out.println("I am just trying to understand what is going on here");
         System.out.println("I am making some changes");
+
+        // do you want some more changes
     }
 }
