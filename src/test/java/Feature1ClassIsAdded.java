1fb11ff368b8733263490bb7b012959e4ec9a1ef
Caglar Keskin <caglarkeskin@gmail.com>
2020-10-09 12:27:02 +0200

    feature1 branch is added something

---
+++ src/test/java/Feature1ClassIsAdded.java
@@ -0,0 +1,9 @@
+public class Feature1ClassIsAdded {
+
+    public static void main(String[] args) {
+        //I am working o a new project which has no effect on the master branch yet.
+
+    }
+
+
+}
